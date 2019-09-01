b6f353eb2254d7db7f5565d350f9d6f32661b9f4
943161238@qqcom <943161238@qq.com>
2019-09-01 23:31:08 +0800

    03-tab栏练习

---
+++ pages/tab.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="tab">
+      <div class="title">
+          <span v-for="(val,index) in ['新闻','体育','娱乐']" 
+          :key="index" @click = "handleclidk(index)" :class="{active:current===index}">
+          {{val}}
+          </span>
+      </div>
+      <div class="content" v-show="current===0">
+          <ul>
+              <li>123</li>
+              <li>123</li>
+              <li>123</li>
+          </ul>
+      </div>
+      <div class="content" v-show="current===1">
+          <ul>
+              <li>456</li>
+              <li>456</li>
+              <li>456</li>
+          </ul>
+      </div>
+      <div class="content" v-show="current===2">
+          <ul>
+              <li>789</li>
+              <li>789</li>
+              <li>789</li>
+          </ul>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            current : 0
+        }
+    },
+
+    methods: {
+        handleclidk(index){
+            this.current = index
+        }
+    }
+
+}
+</script>
+
+<style lang="less" scoped>
+        .tab {
+            margin: 50px 0 0 50px;
+            .title {
+                cursor: pointer;
+                span {
+                    padding: 0 10px
+                }
+            }
+            .content {
+                margin: 20px 0 0 40px;
+                ul {
+                    li {
+                        padding: 10px;
+                    }
+                }
+            }
+        }
+        .active {
+            border-bottom: 1px solid red;
+        }
+</style>
