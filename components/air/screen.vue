45162b7520e9a37a674b5b5d79868a246bdbdcaa
943161238@qqcom <943161238@qq.com>
2019-09-08 16:56:08 +0800

    25-机票的筛选器功能完成---起飞时间未完成

--- components/air/screen.vue
+++ components/air/screen.vue
@@ -1,46 +1,66 @@
 <template>
   <el-row type="flex" justify="space-between" class="screen">
     <el-col :span="8">
-      <p>单程： {{data.departCity}} - {{data.destCity}} / {{data.departDate}}</p>
+      <p>单程： {{data.info.departCity}} - {{data.info.destCity}} / {{data.info.departDate}}</p>
       <span>筛选:</span>
-      <el-button type="primary" plain round size="mini">撤销</el-button>
+      <el-button type="primary" plain round size="mini" @click="handelClear">撤销</el-button>
     </el-col>
     <el-col :span="4" class="selector_data">
-      <el-select size="mini" v-model="value" placeholder="起飞机场">
+      <el-select
+        size="mini"
+        placeholder="起飞机场"
+        v-model="flightsList.airport"
+        @change="handelAirport"
+      >
         <el-option
-          v-for="item in options"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value"
+          v-for="(item,index) in data.options.airport"
+          :key="index"
+          :label="item"
+          :value="item"
           class="selecter_content"
         ></el-option>
       </el-select>
     </el-col>
     <el-col :span="4" class="selector_data">
-      <el-select size="mini" v-model="value" placeholder="起飞时间">
+      <el-select
+        size="mini"
+        v-model="flightsList.flightTimes"
+        placeholder="起飞时间"
+        @change="handleDepTime"
+      >
         <el-option
-          v-for="item in options"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value"
+          v-for="(item,index) in data.options.flightTimes"
+          :key="index"
+          :label="`${item.from}:00-${item.to}:00`"
+          :value="`${item.from},${item.to}`"
         ></el-option>
       </el-select>
     </el-col>
     <el-col :span="4" class="selector_data">
-      <el-select size="mini" v-model="value" placeholder="航空公司">
+      <el-select
+        size="mini"
+        v-model="flightsList.company"
+        placeholder="航空公司"
+        @change="handleCompany"
+      >
         <el-option
-          v-for="item in options"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value"
+          v-for="(item,index) in data.options.company"
+          :key="index"
+          :label="item"
+          :value="item"
         ></el-option>
       </el-select>
     </el-col>
     <el-col :span="4" class="selector_data">
-      <el-select size="mini" v-model="value" placeholder="机型">
+      <el-select
+        size="mini"
+        v-model="flightsList.airModel"
+        placeholder="机型"
+        @change="handleAirsize"
+      >
         <el-option
-          v-for="item in options"
-          :key="item.value"
+          v-for="(item,index) in airModelInfo"
+          :key="index"
           :label="item.label"
           :value="item.value"
         ></el-option>
@@ -61,9 +81,82 @@ export default {
   },
   data() {
     return {
-      value: "",
-      options: []
+      // 每个选择器所绑定的数据
+      flightsList: {
+        airport: "", //起飞机场
+        flightTimes: "", //  起飞时间
+        company: "",
+        airModel: "" //机型
+      },
+
+      // 机型数据
+      airModelInfo: [
+        {
+          label: "大",
+          value: "L"
+        },
+        {
+          label: "中",
+          value: "M"
+        },
+        {
+          label: "小",
+          value: "S"
+        }
+      ]
     };
+  },
+
+  methods: {
+    // 点击起飞机场时，触发的函数
+    handelAirport(val) {
+      // 筛选出匹配的机场名
+      const arr = this.data.flights.filter(e => {
+        return e.org_airport_name === val;
+      });
+      // 调用父组件的方法
+      this.$emit("setFlightsItem", arr);
+    },
+
+    // 点击起飞时间时出触发的函数
+
+    handleDepTime() {
+      //  console.log(val); //比如6，12
+      //  const Hour = val.split(',')
+      //  const dep_time = this.data.flights.map(e=>{
+      //    return e.dep_time.split('')
+      //})
+    },
+
+    // 选择航空公司时触发的函数
+    handleCompany(val) {
+      // console.log(val)
+      // 从数组中筛选出机场航空名称符合的数据，重新赋值到另一个数组中
+      const arr = this.data.flights.filter(e => {
+        return e.airline_name === val;
+      });
+      this.$emit("setFlightsItem", arr);
+      console.log(arr);
+    },
+
+    // 点击选择机型时触发的函数
+    handleAirsize(val) {
+      console.log(val);
+      // 筛选出机型匹配的内容
+      const arr = this.data.flights.filter(e => {
+        return e.plane_size === val;
+      });
+      // 调用组件的方法
+      this.$emit("setFlightsItem", arr);
+    },
+
+    // 点击撤销按钮时触发的函数
+    handelClear(){
+      console.log(123)
+      for(var key in this.flightsList){
+        this.flightsList[key] = ""
+      }
+    }
   }
 };
 </script>
--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -2,58 +2,66 @@
   <div class="container">
     <el-row type="flex" justify="space-between">
       <div class="left">
-      <!-- 过滤器 -->
-    <Screen :data= flightsList.info></Screen>
-      <!-- 航班信息标题 -->
-      <div>
-        <FlightsHeader />
-        <!-- 航班信息内容 -->
-        <FlightsItems v-for="(item,index) in dataList" :key="index" :data="item" />
-        <!-- 分页标签 -->
-        <!-- size-change 切换页面数时调用的函数 
+        <!-- 过滤器 -->
+        <Screen :data="cacheList" @setFlightsItem="setFlightsItem"></Screen>
+        <!-- 航班信息标题 -->
+        <div>
+          <FlightsHeader />
+          <!-- 航班信息内容 -->
+          <FlightsItems v-for="(item,index) in dataList" :key="index" :data="item" />
+          <!-- 分页标签 -->
+          <!-- size-change 切换页面数时调用的函数 
         current-change  点击当前页数时调用函数
-        -->
-        <el-pagination
-          @size-change="handleSizeChange"
-          @current-change="handleCurrentChange"
-          :current-page="pageIndex"
-          :page-sizes="[5, 10, 15, 20]"
-          :page-size="pageSize"
-          layout="total, sizes, prev, pager, next, jumper"
-          :total="total"
-        ></el-pagination>
+          -->
+          <el-row type="flex" justify="center">
+             <el-pagination
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page="pageIndex"
+            :page-sizes="[5, 10, 15, 20]"
+            :page-size="pageSize"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="total"
+          ></el-pagination>
+          </el-row>
+         
+        </div>
       </div>
-</div>
       <!-- 右侧航班历史记录 -->
-      <Aside/>
+      <Aside />
     </el-row>
   </div>
 </template>
 
 <script>
-import Screen from '@/components/air/screen'
+import Screen from "@/components/air/screen";
 import FlightsHeader from "@/components/air/flightsHeader";
 import FlightsItems from "@/components/air/flightsItems";
-import Aside from '../../components/air/airAside'
+import Aside from "../../components/air/airAside";
 
 export default {
   data() {
     return {
-        dataList: [],
-        // 当前显示的页面数
-        pageSize: 5,
-        // 当前页面
-        pageIndex: 1,
-        // 总的记录数
-        total: 0,
-        // 这是一个总数据
+      dataList: [],
+      // 当前显示的页面数
+      pageSize: 5,
+      // 当前页面
+      pageIndex: 1,
+      // 总的记录数
+      total: 0,
+      // 这是一个总数据
       flightsList: {
-        info: {}
+        info: {},
+        options: {}
+      },
+
+      // 需要一个不会改变的总数据传值给子组件
+      cacheList: {
+        info: {},
+        options: {}
       }
     };
   },
-
-
 
   // 注册组件
   components: {
@@ -63,31 +71,42 @@ export default {
     Aside
   },
 
+  methods: {
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`); //比如每页 10 条
+      this.pageSize = val;
+      // 切换页数时显示的数据
+      this.dataList = this.flightsList.flights.slice(0, val);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`); //当前页面1
 
+      // 0,5 (1-1)*5,5
+      // 5,10(2-1)*5
+      // 10,15(3-1)*5
+      this.pageIndex = val;
+      this.dataList = this.flightsList.flights.slice(
+        (this.pageIndex - 1) * this.pageSize,
+        this.pageIndex * this.pageSize
+      );
+    },
 
-methods: {
-     handleSizeChange(val) {
-    console.log(`每页 ${val} 条`);   //比如每页 10 条
-    this.pageSize = val
-    // 切换页数时显示的数据
-    this.dataList = this.flightsList.flights.slice(0,val)
+    // 传值给组件的事件
+    setFlightsItem(arr) {
+      
+      // 每次点击切换时，还要把页码数切换到第一页
+      this.pageIndex = 1
+      // console.log(val)
+      this.flightsList.flights = arr;
+      this.dataList = this.flightsList.flights.slice(
+        (this.pageIndex - 1) * this.pageSize,
+        this.pageIndex * this.pageSize
+      )
+      // 总页数要重新复制
+      this.total = this.flightsList.flights.length;
+      
+    }
   },
-  handleCurrentChange(val) {
-    console.log(`当前页: ${val}`);  //当前页面1
- 
-    // 0,5 (1-1)*5,5
-    // 5,10(2-1)*5
-    // 10,15(3-1)*5
-    this.pageIndex = val
-    this.dataList = this.flightsList.flights.slice((this.pageIndex-1)*this.pageSize,
-    this.pageIndex*this.pageSize)
-    
-  },
-},
- 
-
-
-
 
   mounted() {
     //   获取路由地址上的参数
@@ -98,13 +117,16 @@ methods: {
       params: this.$route.query
     }).then(res => {
       console.log(res);
+      // 这是总数据,里面有四个选项
       this.flightsList = res.data;
-    //   分页总页数
-    this.total = this.flightsList.flights.length
 
-        //   复制给另一个数组，这是显示当前页面数据的数组
-    this.dataList = this.flightsList.flights.slice(0,this.pageSize)
+      // 这是为了方便给子组件传递数组，并且是不会改变的数据
+      this.cacheList = { ...res.data };
+      //   分页总页数
+      this.total = this.flightsList.flights.length;
 
+      //   复制给另一个数组，这是显示当前页面数据的数组
+      this.dataList = this.flightsList.flights.slice(0, this.pageSize);
     });
   }
 };
@@ -119,4 +141,6 @@ methods: {
 .aside {
   width: 240px;
 }
+
+
 </style>
